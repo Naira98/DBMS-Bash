5b5504c1c60a37923be083e567dc7e0a2b4d1f87
Naira98 <nairamm99@gmail.com>
2025-05-07 20:00:21 +0000

    refactor function

--- create_db.sh
+++ create_db.sh
@@ -15,14 +15,12 @@ if [[ $status -eq 0 ]]; then
         #Create the database directory
         mkdir -p "$SCRIPT_DIR/$WORK_SPACE/$result"
         print_success "Database '$result' created successfully."
-        clear_terminal "$SCRIPT_DIR/main_menu.sh"
     else
         print_error "Database '$result' already exists."
-        clear_terminal "$SCRIPT_DIR/main_menu.sh"
     fi
-    
 else
     print_error "$result"
-    clear_terminal "$SCRIPT_DIR/main_menu.sh"
 fi
+
+clear_terminal "$SCRIPT_DIR/main_menu.sh"
 
