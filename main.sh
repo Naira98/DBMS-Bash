0bb3e3bb8c71e35f17f6cd3402ba7bfd15ff74ab
Naira98 <nairamm99@gmail.com>
2025-05-07 17:22:58 +0000

    make varibales file

--- main.sh
+++ main.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
-
 # To start project $ source ./main.sh
 
-work_space='DBMS'
+source ./variables.sh
 
-if [[ ! -d $work_space ]]; then
-    mkdir ./$work_space
-    echo $work_space created successfully
+if [[ ! -d $WORK_SPACE ]]; then
+    mkdir ./$WORK_SPACE
+    echo $WORK_SPACE created successfully
 else
-    echo $work_space already exists. Let\'s start working on it.
+    echo $WORK_SPACE already exists. Let\'s start working on it.
 fi
 
 sleep 0.5
---
+++ variables.sh
@@ -0,0 +1,4 @@
+WORK_SPACE='DBMS'
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
