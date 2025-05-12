16f15e8b30ee99db3b3da9e70079c05718a96dad
Naira98 <nairamm99@gmail.com>
2025-05-12 10:54:02 +0000

    add drop table functionality

--- table_scripts/drop_table.sh
+++ table_scripts/drop_table.sh
@@ -1,22 +1,13 @@
 #! /bin/bash
 
 set -e
+source ./utils/select_from_tables_utils.sh
 source ./utils/output_utils.sh
 source ./utils/validate_utils.sh
 source ./utils/constants.sh
 
-read -rp "Enter the name of the table you want to drop: " table_name
-
-# Input Validation
-table_name=$(validate_name "$table_name" "Table") 
-
-# Existence Validation
-FILE_PATH="./$WORK_SPACE/$CONNECTED_DB/$table_name"
-METADATA_PATH="./$WORK_SPACE/$CONNECTED_DB/.$table_name"
-error_message="Table '$table_name' doesn't exist."
-validate_file_exists "$FILE_PATH" "$error_message"
+table_name=$(select_from_tables "drop")
 
 #Create the table directory
-rm "$FILE_PATH"
-rm "$METADATA_PATH"
+rm -f "$FILE_PATH" "$METADATA_PATH"
 print_green "Table '$table_name' successfully dropped."
