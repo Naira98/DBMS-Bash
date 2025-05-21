a9893b61d20767905bb1fe460846fbd3de9353c7
Naira98 <nairamm99@gmail.com>
2025-05-21 12:17:50 +0000

    check num of matched rows before printing table

--- table_queries_scripts/select.sh
+++ table_queries_scripts/select.sh
@@ -15,9 +15,16 @@ while true; do
     select cols in "All Columns" "Some Columns"; do
         case $cols in
             "All Columns")
-                headers=$(get_table_headers "all")
                 matched_rows=$(ask_for_condition "all" "select" "$query")
+
+                matched_rows_length=$(awk -F" " '{if ($1 != "") count+=1} END {print count}' <<< "$matched_rows")
+
+                if [[ "$matched_rows_length" -eq 0 ]]; then
+                    print_red "Error: There is no matched rows to select"
+                    exit 1
+                fi
                 
+                headers=$(get_table_headers "all")
                 content=$(echo -e "$headers\n$matched_rows")
 
                 print_table "$content"
@@ -26,10 +33,18 @@ while true; do
 
             "Some Columns")
                 chosen_cols_nums=$(ask_for_some_columns)
-                headers=$(get_table_headers "${chosen_cols_nums}")
+
                 matched_rows=$(ask_for_condition "${chosen_cols_nums}" "select" "$query")
 
+                matched_rows_length=$(awk -F" " '{if ($1 != "") count+=1} END {print count}' <<< "$matched_rows")
 
+                if [[ "$matched_rows_length" -eq 0 ]]; then
+                    print_red "Error: There is no matched rows to select"
+                    exit 1
+                fi
+
+
+                headers=$(get_table_headers "${chosen_cols_nums}")
                 content=$(echo -e "$headers\n$matched_rows")
 
                 print_table "$content"
